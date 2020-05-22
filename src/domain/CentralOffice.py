a149d4db90cab235440b34e4bb924dd6fa82bfda
Vaios Laskarelias <vaios.lask@gmail.com>
2020-05-22 20:25:46 +0300

    following domain model changes: AccessLog, CentralOffice, SecurityIncident

--- src/domain/CentralOffice.py
+++ src/domain/CentralOffice.py
@@ -1,5 +1,22 @@
+from datetime import datetime
+
 class CentralOffice:
-	@staticmethod
-	def notifyIncorrectPin(time, pin, sector):
-		print(f'time: {time}, pin: {pin}, sector: {sector}')
-		# send to some random server
+    @staticmethod
+    def notifyIncorrectPin(time, pin, sector):
+        print(f'time: {time}, pin: {pin}, sector: {sector}')
+        # send to some random server
+
+    def notifyDroneLostConnection():
+        time = datetime.now()
+        print(f'Drone Lost Connection at: {time}')
+
+    def notifySilentAlarm(card, reason):
+        time = datetime.now()
+        print(f'!!! Silent Alarm !!!, {time}, {card}, {reason}')
+
+    def sendReport(incident):
+        time = datetime.now()
+        print(f'Anonynmous Incident Report, {incident}, {time}')
+
+
+
